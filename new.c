7011ab93d19849e3ded38aa6e12a1e6ee2c4b258
sathish <sk9209149@gmail.com>
2026-07-02 20:54:25 +0530

    int

--- new.c
+++ new.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() {
-    char d[100];
-    scanf("%s",d);
+    int d[100];
+    scanf("%d",d);
     return 0;
     printf("This line will never be executed.");
     printf("This line will also never be executed.");
